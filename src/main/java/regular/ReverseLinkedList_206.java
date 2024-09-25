9318d2903fd075e110015ded697120b2e3037a6f
Geodakyan Yevgeniy <yevgengeodakyan@gmail.com>
2024-09-25 13:16:37 -0400

    ReverseLinkedList_206

---
+++ src/main/java/regular/ReverseLinkedList_206.java
@@ -0,0 +1,43 @@
+package regular;
+
+public class ReverseLinkedList_206 {
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return val + " " + next;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        ListNode node =
+                new ListNode(1,
+                        new ListNode(2,
+                                new ListNode(3,
+                                        new ListNode(4,
+                                                new ListNode(5,
+                                                        null)))));
+
+        System.out.println(reverseList(node));
+    }
+
+    private static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
+}
