cf43500ce70965116401930261b373fbc793760d
Yevgeniy <yevgengeodakyan@gmail.com>
2024-09-28 11:35:50 -0400

    AddTwoNumbers_2

---
+++ src/main/java/medium/AddTwoNumbers_2.java
@@ -0,0 +1,66 @@
+package medium;
+
+public class AddTwoNumbers_2 {
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return val + " " + next;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+        System.out.println(addTwoNumbers(l1, l2));
+
+    }
+
+    private static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        }
+        int rest = 0;
+        ListNode head = null;
+        ListNode iter = null;
+        while (l1 != null || l2 != null) {
+            int first = l1 == null ? 0 : l1.val;
+            int second = l2 == null ? 0 : l2.val;
+            int sum = first + second + rest;
+            int digit = sum % 10;
+            rest = sum / 10;
+            if (head == null) {
+                head = new ListNode(digit);
+                iter = head;
+            } else {
+                iter.next = new ListNode(digit);
+                iter = iter.next;
+            }
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (rest > 0) {
+            iter.next = new ListNode(rest);
+        }
+        return head;
+    }
+}
