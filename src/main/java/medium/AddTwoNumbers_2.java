dc2a3902b42b4f292974ae9bac2902d8576cbc4a
Yevgeniy <yevgengeodakyan@gmail.com>
2024-09-29 10:40:19 -0400

    AddTwoNumbers_2 updated

--- src/main/java/medium/AddTwoNumbers_2.java
+++ src/main/java/medium/AddTwoNumbers_2.java
@@ -26,6 +26,7 @@ public class AddTwoNumbers_2 {
         ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
 
         System.out.println(addTwoNumbers(l1, l2));
+        System.out.println(addTwoNums(l1, l2));
 
     }
 
@@ -62,5 +63,28 @@ public class AddTwoNumbers_2 {
             iter.next = new ListNode(rest);
         }
         return head;
+    }
+
+    private static ListNode addTwoNums(ListNode l1, ListNode l2) {
+
+        ListNode temp = new ListNode(0);
+        ListNode current = temp;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        return temp.next;
     }
 }
